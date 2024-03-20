08daea34f07d7dc569ac5cef62abc08a3e1198d4
NightWolf200 <94641069+NightWolf200@users.noreply.github.com>
2024-03-20 04:32:50 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { sig } = require("./utils/sig");
 
     await dc.initialize();
     // here is where you enter your email and password
-    await dc.login('email', 'password')
+    await dc.login('davidmateidamian@gmail.com', 'anime3kage')
 
     await dc.likeChannelProcess('server id', 'channel id', 1) // 1 = 1 minute
 
